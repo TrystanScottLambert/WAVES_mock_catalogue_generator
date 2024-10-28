8d2beb907507b5e019cd640e1936e1f67deff607
TrystanScottLambert <trystanscottlambert@gmail.com>
2024-10-28 17:31:24 +0800

    Added Gal/Group properties in README.md

---
+++ make_property_table.py
@@ -0,0 +1,29 @@
+"""
+Script to create the .md table with all the Galaxy/Group properties.
+"""
+
+from typing import TextIO
+from property_dictionaries import GALAXY_PROPERTIES, GROUP_PROPERTIES
+
+
+def write_property_table(file: TextIO, property_dict: dict) -> None:
+    """
+    Adds a .md table of the given property dictionary to the given file object.
+    """
+    file.write('| Property | Description | \n')
+    for key, value in property_dict.items():
+        file.write(f'| `{key}` | {value} | \n')
+
+def main():
+    """
+    Main for scope.
+    """
+    with open('properties.md', 'w', encoding='utf-8') as file:
+        file.write('# Properties \n')
+        file.write('## Galaxy Properties \n')
+        write_property_table(file, GALAXY_PROPERTIES)
+        file.write('## Group Properties \n')
+        write_property_table(file, GROUP_PROPERTIES)
+
+if __name__ == '__main__':
+    main()
--- properties.md
+++ properties.md
@@ -1,18 +1,3 @@
-# WAVES_mock_catalogue_generator
-This is the official code base to construct WAVES mock catalogues from SHARK/StingRay runs. SHARK and StingRay produce the physcical data and stores these in hdf5 formats on Setonix. This code then scrapes those hdf5 files and produces a usable data product (mock catalogue) for the WAVES colloboration. 
-
-Currenlty this code is in active development in preparation for WAVES and is not yet in its matured form.
-
-To run the code simply install the repo on setonix and run the make_galaxy catlog.py script.
-
-# Tests
-Run the following command to execute the unit tests:
-
-```
-python3 -m unittest discover -s tests -p "test_*.py" # all tests
-python3 -m unittest discover -s tests -p "test_write.py" # specific
-```
-
 # Properties 
 ## Galaxy Properties 
 | Property | Description | 
