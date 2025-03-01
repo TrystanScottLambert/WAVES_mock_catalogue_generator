529bc206886c988d8a44169bc260357ec6bb2036
TrystanScottLambert <trystanscottlambert@gmail.com>
2025-03-01 12:55:43 +0800

    Refacotr make + test. Passing. Not complete.

--- make_galaxy_catalog.py
+++ make_galaxy_catalog.py
@@ -4,13 +4,22 @@ Creating a mock catalogue from the SHARK runs on pawsey.
 
 import numpy as np
 
-from load import load_all
+from load import load_all, Config
 from read import read_lightcone, read_photometry_data_hdf5
 from write import write_to_parquet
 from property_dictionaries import GALAXY_PROPERTIES, GROUP_PROPERTIES
 from table_formats import GalaxyTable, GroupTable
 
-
+def filter_based_on_mag(config_object: Config, sed_data: dict, galaxy_data: dict) -> dict:
+    """
+    Based on the magnitude limit stored in the config_object we filter the galaxy and sed data.
+    """
+    mag_filter = config_object.cat_details.mag_filter
+    mag_limit = config_object.cat_details.mag_cut
+    cut_idxs = np.where(sed_data[mag_filter] < mag_limit)[0]
+    cut_sed_data = {key: value[cut_idxs] for key, value in sed_data.items()}
+    cut_galaxy_data = {key: value[cut_idxs] for key, value in galaxy_data.items()}
+    return cut_sed_data, cut_galaxy_data
 
 def main():
     """
@@ -23,14 +32,7 @@ def main():
     group_data = read_lightcone(config, "group")
     _, sed_data = read_photometry_data_hdf5(config)
 
-    # filtering the data
-    cat_details = config.cat_details
-    indicies = np.where(
-        (sed_data[cat_details.mag_filter] < cat_details.mag_cut)
-        & (sed_data[cat_details.mag_filter] > 0)
-    )[0]
-    sed_data = {key: value[indicies] for key, value, in sed_data.items()}
-    galaxy_data = {key: value[indicies] for key, value in galaxy_data.items()}
+    sed_data, galaxy_data = filter_based_on_mag(config, sed_data, galaxy_data)
 
     # Working out calculated properties
     galaxy_data = GalaxyTable(galaxy_data, GALAXY_PROPERTIES, config.cosmo)
@@ -46,11 +48,11 @@ def main():
     write_to_parquet(
         [galaxy_data_to_write, sed_data],
         galaxy_header,
-        cat_details,
+        config.cat_details,
         config.galaxy_outfile_name,
     )
     write_to_parquet(
-        [group_data_to_write], group_header, cat_details, config.group_outfile_name
+        [group_data_to_write], group_header, config.cat_details, config.group_outfile_name
     )
 
 
