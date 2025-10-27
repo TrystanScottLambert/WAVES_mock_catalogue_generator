3e19b48f78b56cd41d7c0167df95d2ed89a27e41
TrystanScottLambert <trystanscottlambert@gmail.com>
2025-10-27 14:19:55 +0800

    Adding the group object + overlap + tests. ATP

--- group_post_process.py
+++ group_post_process.py
@@ -3,8 +3,11 @@ Module for post-processing group ids
 """
 
 from typing import Union
+from dataclasses import dataclass
+
 from astropy.cosmology import Planck18
 from astropy.units import Quantity
+from astropy.coordinates import SkyCoord
 import astropy.units as u
 import numpy as np
 
@@ -23,5 +26,34 @@ def calc_rvir_from_mvir(
     """
     numerator = 3 * mvir
     denominator = 4 * np.pi * 200 * Planck18.critical_density(redshift)
-    rvir_units = (numerator/denominator) ** (1 / 3)
+    rvir_units = (numerator / denominator) ** (1 / 3)
     return rvir_units.to(u.Mpc).value
+
+
+@dataclass
+class Group:
+    """
+    Data class representing a group.
+    """
+    mass: float
+    ra: float
+    dec: float
+    zcos: float
+
+    def __post_init__(self):
+        self.rvir = calc_rvir_from_mvir(self.mass * u.solMass, self.zcos)
+        distance = Planck18.comoving_distance(self.zcos)
+        c = SkyCoord(ra=self.ra * u.deg, dec=self.dec * u.deg, distance=distance)
+        self.center = np.array(
+            [c.cartesian.x.value, c.cartesian.y.value, c.cartesian.z.value]
+        )
+
+    def overlap(self, other_group: "Group") -> bool:
+        """
+        Determins group is overlapping with other_group
+        """
+        difference = self.center - other_group.center
+        distance_between_centers = np.sqrt((difference[0])**2 + (difference[1])**2 + (difference[2])**2)
+        total_radii = self.rvir + other_group.rvir
+        overlapped = distance_between_centers < total_radii
+        return overlapped
--- tests/test_group_postprocess.py
+++ tests/test_group_postprocess.py
@@ -6,7 +6,7 @@ import unittest
 import astropy.units as u
 import numpy as np
 
-from group_post_process import calc_rvir_from_mvir
+from group_post_process import calc_rvir_from_mvir, Group
 
 
 class TestMvir(unittest.TestCase):
@@ -34,6 +34,34 @@ class TestMvir(unittest.TestCase):
         answers = [0.18987214, 1.97123138]
         for res, ans in zip(results, answers):
             self.assertAlmostEqual(res, ans, places=4)
+
+
+class TestOverlap(unittest.TestCase):
+    """
+    Testing that the overlap method in a group works.
+    """
+
+    group_1 = Group(1e15, 0.0, 0.0, 0.2)
+    group_2 = Group(1e15, 0.0, 0.0, 0.2)
+    group_3 = Group(1e15, 180, 0, 0.2)
+    group_4 = Group(1e15, 0.0, 0.0, 1)
+
+    def test_trivial_case(self):
+        """test true for same group"""
+        self.assertTrue(self.group_1.overlap(self.group_2))
+        self.assertTrue(self.group_2.overlap(self.group_1))
+
+    def test_on_sky_sep(self):
+        """
+        Testing fails when we separate group on sky.
+        """
+        self.assertFalse(self.group_1.overlap(self.group_3))
+
+    def test_behind(self):
+        """
+        Testing fail when groups are behind one another.
+        """
+        self.assertFalse(self.group_1.overlap(self.group_4))
 
 
 if __name__ == "__main__":
