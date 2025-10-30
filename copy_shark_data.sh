c93963431b5bb3fd84af874a6d24c95cd005fd73
TrystanScottLambert <trystanscottlambert@gmail.com>
2025-10-30 10:07:26 +0800

    Adding rclone scripts

---
+++ copy_shark_data.sh
@@ -0,0 +1,4 @@
+module load rclone/1.68.1
+
+rclone copy pawsey0119:clagos/Stingray/medi-SURFS/Sharkv2-Lagos23-HBTTrees-bestparams/waves-wide/ /scratch/pawsey0335/tlambert/mock_catalogues/waves-wide/
+rclone copy pawsey0119:clagos/Stingray/medi-SURFS/Sharkv2-Lagos23-HBTTrees-bestparams/waves-deep/ /scratch/pawsey0335/tlambert/mock_catalogues/waves-deep/
